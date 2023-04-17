e2fb8b09185010a0e386e97f04ea2479d591f040
Ed Harcourt <edharcourt@stlawu.edu>
2023-04-16 21:26:38 -0400

    .

---
+++ encoding/add.s
@@ -0,0 +1,8 @@
+.text
+
+f:
+    add r3, r4, r0, lsl #2
+    add r4, r2, #99
+    ands r0, r1, #7
+    addeq r1, r1, #1
+
---
+++ encoding/cond.c
@@ -0,0 +1,6 @@
+int f(int x) {
+    if (x % 8 == 0)
+        x = x + 1;
+    return x;
+}
+
