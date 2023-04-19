d8aad4adea28f51bf778592a174a65c5f32922d3
Ed Harcourt <edharcourt@stlawu.edu>
2023-04-19 14:07:33 -0400

    .

---
+++ instr_encoding/add.s
@@ -0,0 +1,10 @@
+.text
+.syntax unified
+
+f:
+  add r3, r4, r0, lsl #2
+  add r3, r4, r0
+  add r4, r2, #99
+  adds r4, r2, #255
+  eorslt r1,r8,#62
+  sub r1, r2, r3, asr #2
---
+++ instr_encoding/ldr.s
@@ -0,0 +1,5 @@
+.syntax unified
+
+f:
+  ldr r1, [r0, #4]
+  ldrlt r1, [r0, r2]
