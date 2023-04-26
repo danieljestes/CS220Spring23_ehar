21d2f936e3e1610b4dcde1921cad15d82c74924d
Ed Harcourt <edharcourt@stlawu.edu>
2023-04-25 20:07:42 -0400

    .

--- encoding/add.s
+++ encoding/add.s
@@ -1,8 +1,11 @@
 .text
+.syntax unified
 
 f:
     add r3, r4, r0, lsl #2
     add r4, r2, #99
     ands r0, r1, #7
     addeq r1, r1, #1
-
+    addsge r1, sp, #254 
+    mulslt r3, r4, r5
+    addsge r1, sp, #254
